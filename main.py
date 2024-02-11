60327ab80f33987f2207e4d66e3308e16b5c6ba5
Flailgull <flailgull@gmail.com>
2024-02-11 18:08:23 +0100

    read the frankenstein.txt from directory books and print it to the console

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("hello world")
+FRANKENSTEIN = "books/frankenstein.txt"
+def main():
+    with open(FRANKENSTEIN, "r") as f:
+        file_contents = f.read()
+        print(file_contents)
+        
+main()
