e64b2abd59f422a5937098a0cc18530715f52dcb
Flailgull <flailgull@gmail.com>
2024-02-11 18:16:30 +0100

    counting words - add get_word_count() function, add commentary to main()

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 FRANKENSTEIN = "books/frankenstein.txt"
 def main():
+    print("===separator===")
+    print("Reading books/frankenstein.txt and printing it to the console")
     text = get_book_text(FRANKENSTEIN)
     print(text)
+    
+    print("===separator===")
+    print("Calling 'get_word_count()' with the frankenstein text. Expected result: 77986 words.")
+    word_count = get_word_count(text)
+    print(word_count)
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+def get_word_count(text):
+    return len(text.split())
         
 main()
